d2e67a37695cdff5db24ac501001f9ea0a26df84
Raghul S <raghuls@in.megatrends.com>
2017-08-10 14:11:09 +0530

    Changes

---
+++ lists.py
@@ -0,0 +1,28 @@
+student_names=[]  //declaring a list
+student_names=['Mark',"Raghul","shiv"] //List elements
+students_names[0]== //accessing through index
+students_names[2]==shiv //aacessing through
+We can access through  negative index values as -1 -2 -3
+we can get last element easily by using negative index values.
+students_names[-1]==shiv
+students_names[-2]==Raghul//accessing elements through negative index value.
+
+Replace a names in list
+students_names[1]=Rocky //Student name in index 1 wil be replaced.
+To add elements in existing list
+students_nappend("Homer")//Add element in existing list or
+students_names[3]="Homer"
+
+To check element present in list.
+
+"Raghul" in students_names== True
+To find Elements present in list // use buit -in function  "len"
+len(student_list) ==4 
+//Delete a element list
+del students_names[2]shiv will be deleted
+
+Slicing of elements in list
+students_names = ["Mark " ,"Raghul" ,"Homer"]
+students_names[1:]==["Raghul","Homer"]//Slicing with index values(i.e it will ignore the first element)
+sudents_names[1:-1] == ["Raghul"]//slicing with index values (i.e it will ignore first and lat element)
+
---
+++ looping.py
@@ -0,0 +1,14 @@
+2 loops in python
+for loop and while loop
+
+for loop using index .
+for name in students_names:
+    print ("student_names is{0}".format(name))
+    
+To print loop in 10 times without list . we will be using range
+x=0
+for index in range(10)
+x+= 10
+print("The value of X is {0}".format(x)) 
+
+
